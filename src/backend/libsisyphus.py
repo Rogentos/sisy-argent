1a4e257d71aa5caefb078f6e21847d30b1994ccd
V3n3RiX <venerix@redcorelinux.org>
2017-09-30 20:16:20 +0100

    rescue fix

--- src/backend/libsisyphus.py
+++ src/backend/libsisyphus.py
@@ -258,8 +258,6 @@ def sisyphus_db_rescue():
         os.remove(sisyphus_remote_csv_path_pre)
     if os.path.exists(sisyphus_removable_csv_path_pre):
         os.remove(sisyphus_removable_csv_path_pre)
-    if os.path.exists(sisyphus_local_csv_path_pre):
-        os.remove(sisyphus_local_csv_path_pre)
     if os.path.exists(sisyphus_database_path):
         os.remove(sisyphus_database_path)
     sisyphus_pkg_system_update()
