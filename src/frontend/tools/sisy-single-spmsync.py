b0a96b888992b3a6496bcdc9da526d8ea7ffcec5
StefanCristian <steven.darklight@gmail.com>
2017-12-13 01:00:22 +0200

    Added sisy-single-spmsync{.py} that introduces specific-package spmsync between portage DB and general sisyphus spmsync [too many sisy's!]

--- src/frontend/tools/sisy-single-spmsync.py
+++ src/frontend/tools/sisy-single-spmsync.py
@@ -2,6 +2,8 @@
 
 from libsisyphus import *
 
+sisyphus_singlepkg_spm_csv_path = '/var/lib/sisyphus/csv/portage_singlepkg_spmsync.csv'
+
 def sync_sisyphus_single_spm_csv():
     sisyphusdb = sqlite3.connect(sisyphus_database_path)
     with open(sisyphus_spm_csv_path) as sisyphus_spm_csv:
@@ -14,9 +16,31 @@ def sync_sisyphus_single_spm_csv():
         sisyphusdb.close()
     os.remove(sisyphus_spm_csv_path)
 
+def generate_sisyphus_spm_csv():
+    subprocess.check_call(['/usr/share/sisyphus/helpers/make_singlepkg_spmsync_csv'])
+
+def sync_sisyphus_single_pkg_spm_csv():
+    sisyphusdb = sqlite3.connect(sisyphus_database_path)
+    with open(sisyphus_singlepkg_spm_csv_path) as sisyphus_spm_csv:
+        for row in csv.reader(sisyphus_spm_csv):
+            sisyphusdb.cursor().execute("insert into local_packages (category, name, version, slot, timestamp, description) values (?, ?, ?, ?, ?, ?);", row)
+        sisyphusdb.commit()
+        sisyphusdb.close()
+    os.remove(sisyphus_singlepkg_spm_csv_path)
+
 def sisyphus_pkg_spmsync():
+    generate_sisyphus_spm_csv()
     sync_sisyphus_single_spm_csv()
+
+PKGLIST = sys.argv[2:]
 
 if "__main__" == __name__:
     if "spmsync" in sys.argv[1:]:
+        print("changing values")
         sync_sisyphus_single_spm_csv()
+    elif "spmsync-pkg" in sys.argv[1] and len(sys.argv) >=3:
+        print(sys.argv[0])
+        print(sys.argv[1])
+        print(sys.argv[2:])
+    elif not len(sys.argv) >3:
+        print("blah")
