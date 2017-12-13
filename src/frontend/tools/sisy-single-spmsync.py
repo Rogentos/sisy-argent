63223b6a518c629ef102741dbf95bd01eaf0b5ac
StefanCristian <steven.darklight@gmail.com>
2017-12-13 03:24:10 +0200

    Added verifications to stop the annoying errors of usage, files not found, etc.

--- src/frontend/tools/sisy-single-spmsync.py
+++ src/frontend/tools/sisy-single-spmsync.py
@@ -8,6 +8,9 @@ sisyphus_singlepkg_spm_csv_path = '/var/lib/sisyphus/csv/portage_singlepkg_spmsy
 
 def sync_sisyphus_single_spm_csv():
     sisyphusdb = sqlite3.connect(sisyphus_database_path)
+    if not os.path.isfile(sisyphus_singlepkg_spm_csv_path):
+        print("Please populate the csv file with relevant fields (category, name, version): ", sisyphus_singlepkg_spm_csv_path)
+        os._exit(1)
     with open(sisyphus_singlepkg_spm_csv_path) as sisyphus_spm_csv:
         for row in csv.reader(sisyphus_spm_csv):
             sisyphusdb.cursor().execute(
@@ -41,7 +44,10 @@ def sisyphus_singlepkg_spmsync():
 if "__main__" == __name__:
     if "spmsync" in sys.argv[1:]:
         sync_sisyphus_single_spm_csv()
-    elif "spmsync-pkg" in sys.argv[1] and len(sys.argv) >=3:
+    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) >= 3:
         sisyphus_singlepkg_spmsync()
-    elif not len(sys.argv) >3:
-        print("help sign here")
+    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) >= 2:
+        print("Please specify a package or a list of packages")
+        os._exit(1)
+    elif len(sys.argv) < 2:
+        print("You`ve got two functions: spmsync or spmsync-pkg <pkg>")
