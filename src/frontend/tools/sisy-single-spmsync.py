b5ba08108b16c0ce565daf747ce14c00b18b5752
StefanCristian <steven.darklight@gmail.com>
2017-12-20 21:16:13 +0200

    Added a more sane approach to 'delete a single package from sisy db' based on PN, CATEGORY and PVR variables passed by portage directly.

--- src/frontend/tools/sisy-single-spmsync.py
+++ src/frontend/tools/sisy-single-spmsync.py
@@ -6,11 +6,46 @@ PKGLIST = sys.argv[2:]
 sisyphus_singlepkg_spm_csv_path = '/var/lib/sisyphus/csv/portage_singlepkg_spmsync.csv'
 
 
+# these variables are to be used only if you know what portage is
+def portage_pkg_pn():
+    try:
+        os.environ["PN"]
+    except:
+        pass
+    else:
+        return True
+
+
+def portage_pkg_cat():
+    try:
+        os.environ["CATEGORY"]
+    except:
+        pass
+    else:
+        return True
+
+
+def portage_pkg_pvr():
+    try:
+        os.environ["PVR"]
+    except:
+        pass
+    else:
+        return True
+
+
+def check_portage_binpkg_vars():
+    try:
+        portage_pkg_pn() and portage_pkg_cat() and portage_pkg_pvr()
+    except:
+        pass
+
+
 def sync_sisyphus_single_spm_csv():
     sisyphusdb = sqlite3.connect(sisyphus_database_path)
     if not os.path.isfile(sisyphus_singlepkg_spm_csv_path):
         print("Please populate the csv file with relevant fields (category, name, version): ", sisyphus_singlepkg_spm_csv_path)
-        os._exit(1)
+        quit()
     with open(sisyphus_singlepkg_spm_csv_path) as sisyphus_spm_csv:
         for row in csv.reader(sisyphus_spm_csv):
             sisyphusdb.cursor().execute(
@@ -20,6 +55,14 @@ def sync_sisyphus_single_spm_csv():
         sisyphusdb.commit()
         sisyphusdb.close()
     os.remove(sisyphus_singlepkg_spm_csv_path)
+
+
+def sync_sisyphus_singlepkg_db():
+    sisyphusdb = sqlite3.connect(sisyphus_database_path)
+    sisyphusdb.cursor().execute('''DELETE FROM local_packages
+        WHERE category=? AND name=? AND version=?''', (os.environ["CATEGORY"], os.environ["PN"], os.environ["PVR"]))
+    sisyphusdb.commit()
+    sisyphusdb.close()
 
 
 def generate_sisyphus_singlepkg_spm_csv():
@@ -37,17 +80,24 @@ def sync_sisyphus_single_pkg_spm_csv():
 
 
 def sisyphus_singlepkg_spmsync():
-    generate_sisyphus_singlepkg_spm_csv()
-    sync_sisyphus_single_spm_csv()
+    try:
+        check_portage_binpkg_vars()
+    except:
+        generate_sisyphus_singlepkg_spm_csv()
+        sync_sisyphus_single_spm_csv()
+    else:
+        sync_sisyphus_singlepkg_db()
+        if os.path.isfile(sisyphus_singlepkg_spm_csv_path):
+            os.remove(sisyphus_singlepkg_spm_csv_path)
 
 
 if "__main__" == __name__:
     if "spmsync" in sys.argv[1:]:
-        sync_sisyphus_single_spm_csv()
-    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) >= 3:
         sisyphus_singlepkg_spmsync()
-    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) >= 2:
+    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) >= 3:
+        generate_sisyphus_singlepkg_spm_csv()
+        sync_sisyphus_single_pkg_spm_csv()
+    elif "spmsync-pkg" in sys.argv[1:] and len(sys.argv) <= 2:
         print("Please specify a package or a list of packages")
-        os._exit(1)
     elif len(sys.argv) < 2:
         print("You`ve got two functions: spmsync or spmsync-pkg <pkg>")
